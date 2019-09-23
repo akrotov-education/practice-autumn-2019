0e1f9ccef6b9e889c7acce5defc37f7bc5a9a39d
Artem Krotov <akrotov.work@gmail.com>
2019-09-24 02:52:17 +0300

    lesson02
    lesson02

---
+++ src/main/java/lesson01/part1/Task08.java
@@ -0,0 +1,34 @@
+package lesson01.part1;
+
+public class Task08 {
+
+    /**
+     * Реализуй метод getMetreFromCentimetre(int centimetre).
+     * Метод на вход принимает количество сантиметров.
+     * Нужно сделать так, чтобы метод возвращал число полных метров в centimetre. (1 метр = 100 см).
+     * <p>
+     * Пример:
+     * Метод getMetreFromCentimetre вызывается с параметром 243.
+     * <p>
+     * Пример вывода:
+     * 2
+     * <p>
+     * <p>
+     * Требования:
+     * 1. Программа не должна считывать данные с клавиатуры.
+     * 2. Метод getMetreFromCentimetre(int) должен быть публичным и статическим.
+     * 3. Метод getMetreFromCentimetre должен возвращать значение типа int.
+     * 4. Метод getMetreFromCentimetre не должен ничего выводить на экран.
+     * 5. Метод getMetreFromCentimetre должен правильно возвращать количество полных метров в centimetre.
+     */
+
+    public static void main(String[] args) {
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        //напишите тут ваш код
+
+        return 0;
+    }
+}
---
+++ src/main/java/lesson02/part02/Task09.java
@@ -0,0 +1,35 @@
+package lesson02.part02;
+
+/**
+ * Напишите метод displayClosestToTen. Метод должен выводить на экран ближайшее к 10 из двух чисел, записанных в аргументах метода.
+ * Например, среди чисел 8 и 11 ближайшее к десяти 11. Если оба числа на равной длине к 10, то вывести на экран любое из них.
+ * Подсказка: используйте метод public static int abs(int a), который возвращает абсолютную величину числа.
+ * <p>
+ * <p>
+ * Требования:
+ * 1.	Программа должна выводить числа на экран.
+ * 2.	Метод main не должен вызывать System.out.println() или System.out.print().
+ * 3.	Метод main должен вызывать метод displayClosestToTen.
+ * 4.	Метод displayClosestToTen должен вызывать метод abs.
+ * 5.	Метод displayClosestToTen должен выводить число на экран согласно заданию.
+ */
+
+public class Task09 {
+    public static void main(String[] args) {
+        displayClosestToTen(8, 11);
+        displayClosestToTen(7, 14);
+    }
+
+    public static void displayClosestToTen(int a, int b) {
+        // напишите тут ваш код
+
+    }
+
+    public static int abs(int a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+}
