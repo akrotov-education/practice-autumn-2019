ff56eada71f9a343c3475efecfce05c30cb38296
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:35:24 +0300

    Update Task13.java

--- src/main/java/lesson01/part1/Task13.java
+++ src/main/java/lesson01/part1/Task13.java
@@ -29,7 +29,7 @@ public class Task13 {
     }
 
     public static double getWeight(int earthWeight) {
-        //напишите тут ваш код
-        return 0D;
+        double wm = earthWeight * 0.17;
+        return wm;
     }
 }
