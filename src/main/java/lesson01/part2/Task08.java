1ae35f0316c464b25c10398e5ca87635c5d5fce8
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:53:06 +0300

    Update Task08.java

--- src/main/java/lesson01/part2/Task08.java
+++ src/main/java/lesson01/part2/Task08.java
@@ -14,13 +14,29 @@ public class Task08 {
      * 5. В классе Cat не должно быть методов.
      */
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         //напишите тут ваш код
-        Cat cat8 = new Cat();
-        Cat cat9;
-        //напишите тут ваш код
+        Cat cat11 = new Cat();
+        Cat cat12 = new Cat();
+        Cat cat13 = new Cat();
+        Cat cat14 = new Cat();
+        Cat cat15 = new Cat();
+        Cat cat16 = new Cat();
+        Cat cat17 = new Cat();
+        Cat cat18 = new Cat();
+        //8 объектов типа Cat
+        Cat cat21;
+        Cat cat22;
+        Cat cat23;
+        Cat cat24;
+        Cat cat25;
+        Cat cat26;
+        Cat cat27;
+        Cat cat28;
+        Cat cat29;
+        Cat cat210;
+        //10 переменных типа Cat
     }
-
     public static class Cat {
 
     }
