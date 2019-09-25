e4ba7fe0db8001c46b075b415fa20c931f4c2bd5
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:47:17 +0300

    Update Task04.java

--- src/main/java/lesson01/part2/Task04.java
+++ src/main/java/lesson01/part2/Task04.java
@@ -22,8 +22,11 @@ public class Task04 {
      * 6. Каждое значение должно быть выведено с новой строки
      */
 
-    public static void main(String[] args) {
-        //напишите тут ваш код
+   public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println(a+b);
+        System.out.println(a*b);
     }
 
 }
