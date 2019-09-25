6e9f2332afc764e1f4835e87ab95f5ccfe1c0641
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:50:16 +0300

    Update Task07.java

--- src/main/java/lesson01/part2/Task07.java
+++ src/main/java/lesson01/part2/Task07.java
@@ -14,14 +14,14 @@ public class Task07 {
      * 4. Нужно закомментировать некоторые строки и не менять остальные.
      */
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         int x = 1;
         int y = 0;
 
         y = y + 3 * x;
-        x = x * 2;
+        //x = x * 2;
         x = x * 16;
-        y = y + 2 * x;
+        //y = y + 2 * x;
         y = y + x;
 
         System.out.println(y);
