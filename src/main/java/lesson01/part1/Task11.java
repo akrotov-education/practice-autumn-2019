8fc89dfb2e461da653e60f6e458f82a6174a0620
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:31:34 +0300

    Update Task11.java

--- src/main/java/lesson01/part1/Task11.java
+++ src/main/java/lesson01/part1/Task11.java
@@ -21,16 +21,20 @@ public class Task11 {
      * 4. Метод getVolume(int, int, int) должен правильно возвращать количество воды, которое необходимо для наполнения бассейна в литрах.
      */
 
-    private static class Pool {
+     private static class Pool {
         final public int a = 25;
         final protected int b = 25;
         final private int c = 2;
-
         public long getVolume(int a, int b, int c) {
-            //напишите тут ваш код
-            return 0;
+            return (a*b*c*1000);
         }
+    }
 
+    public static void main(String[] args) {
+        Pool pool = new Pool();
+        long poolVolume = pool.getVolume(25, 5, 2);
+        System.out.println(poolVolume);
+    }
     }
 
     public static void main(String[] args) {
