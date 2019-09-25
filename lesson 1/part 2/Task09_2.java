06df144e7115ba51d0ea93752751d8e07d97d5a5
Alina Malginova <39488063+AlinaMalginova1998@users.noreply.github.com>
2019-09-25 20:19:29 +0300

    Add files via upload

---
+++ lesson 1/part 2/Task09_2.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Task09_2 {
+
+    /**
+     * Создай объект типа Cat (кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+     * Присвой каждому животному владельца (owner).
+     * <p>
+     * <p>
+     * Требования:
+     * 1. Программа не должна выводить текст на экран.
+     * 2. В методе main создай объекты типа Cat, Dog, Fish, Woman занеси их ссылки в переменные.
+     * 3. В методе main присвойте каждому животному владельца Woman.
+     * 4. Класс Cat, Dog, Fish должен содержать только одну переменную Woman owner.
+     * 5. Класс Woman не должен содержать переменных.
+     */
+
+    public static void main(String[] args) {
+
+        Cat kisonka = new Cat();
+        kisonka.owner = new Woman();
+        Dog Sobanya = new Dog();
+        Sobanya.owner = new Woman();
+        Fish Rybonka = new Fish();
+        Rybonka.owner = new Woman();
+        Woman PetLover = new Woman();
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
---
+++ lesson 1/part 2/Task10_2.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Task10_2 {
+    /**
+     * Написать функцию, которая возвращает минимум из двух чисел.
+     * <p>
+     * Подсказка:
+     * Нужно написать тело существующей функции min.
+     * <p>
+     * <p>
+     * Требования:
+     * 1. Программа должна выводить текст на экран.
+     * 2. Метод min не должен выводить текст на экран.
+     * 3. Метод main должен вызвать метод min три раза.
+     * 4. Метод main должен выводить на экран результат работы метода min. Каждый раз с новой строки.
+     * 5. Метод min должен возвращать минимальное значение из чисел a и b.
+     */
+
+    public static int min(int a, int b) {
+        int minimum;
+        if (a < b) {
+            minimum = a;
+        } else {
+            minimum = b;
+        }
+        return minimum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(12, 33));
+        System.out.println(min(-20, 0));
+        System.out.println(min(-10, -20));
+    }
+}
