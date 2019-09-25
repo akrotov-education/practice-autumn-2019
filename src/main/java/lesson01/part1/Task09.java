e6d1dc2304ea710758f3cd8d478b78eb2eaf618f
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:28:13 +0300

    Update Task09.java

--- src/main/java/lesson01/part1/Task09.java
+++ src/main/java/lesson01/part1/Task09.java
@@ -25,8 +25,8 @@ public class Task09 {
         System.out.println(sumDigitsInNumber(546));
     }
 
-    public static int sumDigitsInNumber(int number) {
+   public static int sumDigitsInNumber(int number) {
         //напишите тут ваш код
-        return 0;
+        return (number/100)+(number/10%10)+(number%10);
     }
 }
