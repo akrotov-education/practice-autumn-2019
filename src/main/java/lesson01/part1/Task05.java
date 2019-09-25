dacd19a01d630ebc1c7ae5a50032fc5c3caf5b80
homahel <36620636+homahel@users.noreply.github.com>
2019-09-25 22:20:35 +0300

    Update Task05.java

--- src/main/java/lesson01/part1/Task05.java
+++ src/main/java/lesson01/part1/Task05.java
@@ -12,21 +12,21 @@ public class Task05 {
      * 3. Нельзя изменять (добавлять, удалять) строки с кодом.
      */
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         int a = 3;
         int b = 9;
 
-        System.out.print("девять");
+        //System.out.print("девять");
         System.out.print(b);
         System.out.print(" умножить ");
         System.out.print("на ");
         System.out.print(a);
-        System.out.print("три");
-        System.out.print(" не ");
-        System.out.print("будет ");
+        //System.out.print("три");
+        //System.out.print(" не ");
+        //System.out.print("будет ");
         System.out.print(" равно ");
-        System.out.print("пятнадцать");
-        System.out.print(21);
+        //System.out.print("пятнадцать");
+        //System.out.print(21);
         System.out.print((int) Math.pow(3,3));
     }
 }
