46b4c8f2614ad166089d6ab859d34625f0c1b49c
Alex Lykov <36642211+Sa1ToNice@users.noreply.github.com>
2019-09-28 18:28:54 +0300

    Update Task04.java

--- src/main/java/lesson01/part2/Task04.java
+++ src/main/java/lesson01/part2/Task04.java
@@ -24,6 +24,8 @@ public class Task04 {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println((a+b));
+        System.out.println((a*b));
     }
 
 }
