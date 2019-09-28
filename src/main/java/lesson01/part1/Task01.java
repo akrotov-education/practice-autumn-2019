89a4b2a08cd8c057cf768f9a6893085c471c55e0
Alex Lykov <36642211+Sa1ToNice@users.noreply.github.com>
2019-09-28 15:07:14 +0300

    Update Task01.java

--- src/main/java/lesson01/part1/Task01.java
+++ src/main/java/lesson01/part1/Task01.java
@@ -16,6 +16,7 @@ public class Task01 {
 
     public static void main(String[] args) {
         // Напишите здесь ваш код
-        LOL
+        System.out.println("Hello World!");
+        
     }
 }
