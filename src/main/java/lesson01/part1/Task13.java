ad48cf26e00a87e57c1c6e29ca37c1c580f88417
Alex Lykov <36642211+Sa1ToNice@users.noreply.github.com>
2019-09-28 17:36:35 +0300

    Update Task13.java

--- src/main/java/lesson01/part1/Task13.java
+++ src/main/java/lesson01/part1/Task13.java
@@ -30,6 +30,7 @@ public class Task13 {
 
     public static double getWeight(int earthWeight) {
         //напишите тут ваш код
-        return 0D;
+        double moonWeight = earthWeight * 0.17;
+        return moonWeight;
     }
 }
