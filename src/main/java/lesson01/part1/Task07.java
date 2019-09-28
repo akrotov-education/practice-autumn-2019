edda45b9a9b71c5f00479fd0b099ad775cb4af7d
Alex Lykov <36642211+Sa1ToNice@users.noreply.github.com>
2019-09-28 15:30:07 +0300

    Update Task07.java

--- src/main/java/lesson01/part1/Task07.java
+++ src/main/java/lesson01/part1/Task07.java
@@ -27,7 +27,7 @@ public class Task07 {
 
     public static double convertCelsiusToFahrenheit(int celsius) {
         //напишите тут ваш код
-
-        return 0;
+        double Far = 9./5 * celsius + 32;
+        return Far;
     }
 }
