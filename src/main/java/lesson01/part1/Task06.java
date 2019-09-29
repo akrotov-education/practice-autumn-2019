c6be8d9c77583981888e96644999f51067555b81
Smartkin <vladislav5019@gmail.com>
2019-09-29 17:47:38 +0300

    Complete part 1 of lesson 1

--- src/main/java/lesson01/part1/Task06.java
+++ src/main/java/lesson01/part1/Task06.java
@@ -21,5 +21,8 @@ public class Task06 {
 
     public static void printCircleLength(int radius) {
         //напишите тут ваш код
+        final double Pi = 3.14;
+
+        System.out.println(2 * Pi * radius);
     }
 }
