cd755af76a0fc39cffa53646ab88671d131b9ec2
Azamat Abidokov <Azamat_Abidokov@epam.com>
2019-10-09 10:47:15 +0300

    Added tasks: lesson02

---
+++ src/main/java/lesson02/part01/Task02.java
@@ -0,0 +1,22 @@
+package lesson02.part01;
+
+/**
+ * Добавь метод public static void printString(String s), в нем напиши код, который будет выводить переданную строку на экран.
+ * <p>
+ * <p>
+ * Требования:
+ * 1.	Добавь метод printString, у которого аргумент имеет тип String.
+ * 2.	Метод printString должен быть void.
+ * 3.	Метод printString должен быть static.
+ * 4.	Метод printString должен быть public.
+ * 5.	Метод printString должен выводить переданный текст на экран.
+ * 6.	Программа должна вывести "Hello, Amigo;".
+ */
+
+public class Task02 {
+    // напишите тут ваш код
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
---
+++ src/main/java/lesson02/part01/Task10.java
@@ -0,0 +1,28 @@
+package lesson02.part01;
+
+/**
+ * Реализуй метод public static void writeToConsole(String s), который добавляет к началу строки выражение "printing: "
+ * и выводит измененную строку на экран.
+ * Пример вывода для "Hello world!":
+ * printing: Hello world!
+ * <p>
+ * <p>
+ * Требования:
+ * 1.	Добавь метод writeToConsole, у которого аргумент имеет тип String.
+ * 2.	Метод writeToConsole должен быть void.
+ * 3.	Метод writeToConsole должен быть static.
+ * 4.	Метод writeToConsole должен быть public.
+ * 5.	Метод writeToConsole должен добавлять к началу строки "printing: " и выводить полученный результат на экран.
+ * 6.	Программа должна вывести "printing: Hello world!".
+ */
+
+public class Task10 {
+    public static void main(String[] args) {
+        writeToConsole("Hello world!");
+    }
+
+    public static void writeToConsole(String s) {
+        //напишите тут ваш код
+
+    }
+}
