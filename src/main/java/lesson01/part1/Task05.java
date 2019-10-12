dd78b621e5d1f684b355b6db7633be276f4ff81a
mariti8 <maritimonina8@gmail.com>
2019-10-13 02:01:57 +0300

    clonning full project from main repozitory lesson 1 and 2

--- src/main/java/lesson01/part1/Task05.java
+++ src/main/java/lesson01/part1/Task05.java
@@ -5,11 +5,7 @@ public class Task05 {
     /**
      * Закомментируй ненужные строки кода, чтобы на экран вывелась надпись:
      * 9 умножить на 3 равно 27
-<<<<<<< HEAD
-     *
-=======
      * <p>
->>>>>>> 3c07a3372256f8cb624d3c2a5273a42be9dfa223
      * Требования:
      * 1. Программа должна выводить на экран надпись "9 умножить на 3 равно 27"
      * 2. Нужно раскомментировать одну строку.
@@ -20,19 +16,17 @@ public class Task05 {
         int a = 3;
         int b = 9;
 
-
-        System.out.print("девять");
-        System.out.print(b);
-        System.out.print(" умножить ");
-        System.out.print("на ");
-        System.out.print(a);
-        System.out.print("три");
-        System.out.print(" не ");
-        System.out.print("будет ");
-        System.out.print(" равно ");
-        System.out.print("пятнадцать");
-        System.out.print(21);
-        System.out.print((int) Math.pow(3,3));
-
+        //System.out.print("девять");
+        //System.out.print(b);
+        //System.out.print(" умножить ");
+        //System.out.print("на ");
+        //System.out.print(a);
+        //System.out.print("три");
+        //System.out.print(" не ");
+        //System.out.print("будет ");
+        //System.out.print(" равно ");
+        //System.out.print("пятнадцать");
+        //System.out.print(21);
+        System.out.print((int) Math.pow(3, 2));
     }
 }
