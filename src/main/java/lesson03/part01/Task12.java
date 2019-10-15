70bcd0762a264e6d4cfad09eb2c6e14454fa15e5
Azamat Abidokov <Azamat_Abidokov@epam.com>
2019-10-16 01:59:09 +0300

    Добавил задачи третьего урока

---
+++ src/main/java/lesson03/part01/Task12.java
@@ -0,0 +1,43 @@
+package lesson03.part01;
+
+/**
+ * Изучи внимательно класс Person.
+ * Исправь класс так, чтобы только один метод initialize инициализировал все переменные класса Person.
+ *
+ * Требования:
+ * 1.	Программа не должна считывать данные с клавиатуры.
+ * 2.	У класса Person должно быть 5 переменных.
+ * 3.	У класса должен быть один метод initialize.
+ * 4.	Метод initialize должен иметь пять параметров.
+ */
+
+public class Task12 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+
+    public static class Person {
+        String name;
+        char sex;
+        int money;
+        int weight;
+        double size;
+
+        public void initialize(String name) {
+            this.name = name;
+        }
+
+        public void initialize(String name, char sex) {
+            this.name = name;
+            this.sex = sex;
+        }
+
+        public void initialize(String name, int money, char sex) {
+            this.name = name;
+            this.money = money;
+            this.sex = sex;
+        }
+    }
+
+}
