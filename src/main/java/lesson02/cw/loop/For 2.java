a6ac66e676cdb2360412714d6f89d8d33512aba8
Xesavov <xesavov@yahoo.com>
2019-10-19 23:29:49 +0300

    Lesson01 tests in progress

--- src/main/java/lesson02/cw/loop/For 2.java
+++
@@ -1,9 +0,0 @@
-package lesson02.cw.loop;
-
-public class For {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println();
-        }
-    }
-}
--- src/main/java/lesson02/cw/loop/Iterator 2.java
+++
@@ -1,7 +0,0 @@
-package lesson02.cw.loop;
-
-public class Iterator {
-    public static void main(String[] args) {
-
-    }
-}
