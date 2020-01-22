3d67022adfe56fd1192605437836e9c4400f195a
Lukanina Elena <36620636+homahel@users.noreply.github.com>
2020-01-23 00:03:48 +0300

    lesson05/part01 and part02

---
+++ part01/Task20.java
@@ -0,0 +1,44 @@
+package lesson05.part01;
+
+/**
+ * Пятая правильная «цепочка наследования»
+ * Расставь правильно "цепочку наследования" в классах: House (дом), Cat (кот), Dog (собака), Car (машина), Animal (животное), Asset (имущество).
+ * <p>
+ * <p>
+ * Требования:
+ * 1. Правильно унаследуй класс House.
+ * 2. Правильно унаследуй класс Cat.
+ * 3. Правильно унаследуй класс Dog.
+ * 4. Правильно унаследуй класс Car.
+ * 5. Правильно унаследуй класс Animal.
+ * 6. Правильно унаследуй класс Asset.
+ */
+
+public class Task20 {
+    public static void main(String[] args) {
+    }
+
+    public class House extends Asset{
+
+    }
+
+    public class Cat extends Animal{
+
+    }
+
+    public class Car extends Asset {
+
+    }
+
+    public class Dog extends Animal {
+
+    }
+
+    public class Animal {
+
+    }
+
+    public class Asset {
+
+    }
+}
